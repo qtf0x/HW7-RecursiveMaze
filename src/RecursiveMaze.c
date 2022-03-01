830843b227ad0b5aba8b1f97f064b7c7654c722a
Vincent Marias <vmarias@mines.edu>
2022-03-01 12:07:29 -0700

    implemented printPaths() and stubs for addValueToStack() and printStack()

--- src/RecursiveMaze.c
+++ src/RecursiveMaze.c
@@ -8,9 +8,39 @@
 
 #include <stdio.h>
 
-void printPaths(int* arrAddr, int xMax, int yMax, int curX, int curY,
+void addValueToStack(int numElements, int value);
+
+void printStack(int numElements);
+
+void printPaths(int* grid, int xMax, int yMax, int curX, int curY,
                 int stackLen) {
-    
+    // base case 1
+    if (xMax <= 0 || yMax <= 0) { return; }
+
+    // include current cell in route
+    addValueToStack(stackLen, *(grid + (curX + curY * xMax)));
+    stackLen++;
+
+    // base case 2 (if the last cell is reached)
+    if (curX == xMax - 1 && curY == yMax - 1) {
+        printStack(stackLen);
+        return;
+    }
+
+    // move down
+    if (curY +1 < yMax) {
+        printPaths(grid, xMax, yMax, curX, curY + 1, stackLen);
+    }
+
+    // move right
+    if (curX + 1 < xMax) {
+        printPaths(grid, xMax, yMax, curX + 1, curY, stackLen);
+    }
+
+    // move diagonally
+    if (curX + 1 < xMax && curY + 1 < yMax) {
+        printPaths(grid, xMax, yMax, curX + 1, curY + 1, stackLen);
+    }
 }
 
 int main() {
@@ -18,7 +48,7 @@ int main() {
     char xPrompt[] = "X size: ";
     char yPrompt[] = "Y size: ";
     
-    int arrAddr[sizeof(int) * 200];
+    int grid[sizeof(int) * 200];
     int nextInput = 0;
     int endIndex = 0;
     int xMax = 0;
@@ -27,7 +57,7 @@ int main() {
     printf("%s", arrayNumberPrompt);
     scanf("%d", &nextInput);
     while (nextInput != 0) {
-        *(arrAddr + endIndex) = nextInput;
+        *(grid + endIndex) = nextInput;
 
         printf("%s", arrayNumberPrompt);
         scanf("%d", &nextInput);
@@ -40,7 +70,7 @@ int main() {
     printf("%s", yPrompt);
     scanf("%d", &yMax);
 
-    printPaths(arrAddr, xMax, yMax, 0, 0, 0);
+    printPaths(grid, xMax, yMax, 0, 0, 0);
 
     return 0;
 }
